80dbaa51bd2fae58ba1adf53e11e92b39173bc5c
Fabiana Mello <36797369+bianamello@users.noreply.github.com>
2019-04-11 15:15:38 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,6 +6,6 @@ require 'selenium-webdriver'
 require 'site_prism'
 
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome # roda teste no chrome
+    config.default_driver = :selenium_chrome_headless # roda teste no chrome
     config.default_max_wait_time = 30
 end
