4a4cd9e0ee3d380626a2669900374a5d83943c48
Fabiana <fabiana.mello@dbserver.com.br>
2019-04-12 11:25:04 -0300

    Ajuste no arquivo de script de Pipeline

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,7 +5,29 @@ require 'rspec'
 require 'selenium-webdriver'
 require 'site_prism'
 
+@browser = ENV['BROWSER']
+
+if @browser.eql?('headless')
+    Capybara.javascript_driver = :selenium
+    Capybara.run_server = false
+
+    caps = Selenium::WebDriver::Remote::Capabilities.chrome(
+        'chromeOptions' => {'args' => ['--no-default-browser-check']}
+    )
+
+    Capybara.register_driver :selenium do |app|
+        Capybara::Selenium::Driver.new(
+            app,
+            browser: :remote,
+            url: 'http://alguma_coisa:4444/wd/hub',
+            desired_capabilities: caps
+        )
+    end
+else
+    puts 'executando sem headless'
+end
+
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome_headless # roda teste no chrome
+    config.default_driver = :selenium_chrome # roda teste no chrome
     config.default_max_wait_time = 30
 end
